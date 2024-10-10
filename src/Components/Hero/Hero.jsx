929b1f79d5179cc1b7b7712874dd7ca22b2e0dd1
Maulik Vaghela <maulik.vagh@gmail.com>
2024-10-09 23:22:58 -0400

    Hero Component Updated

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,17 +7,17 @@ import hero_image from '../../Assets/hero_image.png';
 const Hero = () =>{
     return(
         <div className="hero">
-            <div className="her-left">
+            <div className="her0-left">
                 <h2>NEW ARRIVALS ONLY</h2>
                 <div>
-                    <div className="hand-icon">
+                    <div className="hero-hand-icon">
                         <p>new</p>
                         <img src={hand_icon} />
                     </div>
                     <p>collection</p>
                     <p>for everyone</p>
                 </div>
-                <div className="hero-latest">
+                <div className="hero-latest-btn">
                     <div>Latest Collecion</div>
                     <img src={arrow_icon} />
                 </div>
